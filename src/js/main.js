93e9f13e1afcb7d3f1a815be365b9fd99e7d4126
alvikalvik <alvikalvik@gmail.com>
2020-09-18 16:15:13 +0300

    js for menu done

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 // 'use strict';
 
 // import modules
-import testModule from './testmodule';
+import menuModule from './menumodule';
 
 
 // run modules
 
 window.addEventListener('DOMContentLoaded', () => {
     // console.log('DOM fully loaded and parsed');
-    testModule();
+    menuModule();
 });
---
+++ src/js/menumodule.js
@@ -0,0 +1,19 @@
+export default function menuModule() {
+    const hamburger = document.querySelector('.menu__hamburger');
+    const menuList = document.querySelector('.menu__list');
+
+    hamburger.classList.remove('menu__hamburger--active');
+    menuList.classList.remove('menu__list--active');
+
+    hamburger.addEventListener('click', () => {
+        if(hamburger.classList.contains('menu__hamburger--active')) {
+            hamburger.classList.remove('menu__hamburger--active');
+            menuList.classList.remove('menu__list--active');
+        } else {
+            hamburger.classList.add('menu__hamburger--active');
+            menuList.classList.add('menu__list--active');
+        }
+
+    });
+}
+
